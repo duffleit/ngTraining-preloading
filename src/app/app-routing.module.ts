d801fa176557fbe958d527a6c2c75ce1106a9199
David Leitner <david@leitner.io>
2019-04-24 02:20:56 +0200

    Add PreloadAllModules strategy

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, PreloadAllModules } from '@angular/router';
 import { DashboardComponent } from './dashboard/dashboard/dashboard.component';
 import { OrdersComponent } from './orders/orders/orders.component';
 import { CheckoutComponent } from './checkout/checkout/checkout.component';
@@ -29,7 +29,10 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {
+    preloadingStrategy: PreloadAllModules
+
+  })],
   exports: [RouterModule]
 })
 export class AppRoutingModule {}
